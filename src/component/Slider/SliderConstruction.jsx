75abb03aad4d35640fcf93d88a25395840b15f74
Govaerts Yoan <yoangovaerts@gmail.com>
2025-11-28 18:08:20 +0100

    💄 Ajout d'un slider de construction avec animation en défilement et mise à jour des paramètres de réactivité pour une meilleure expérience utilisateur

--- src/component/Slider/SliderConstruction.jsx
+++ src/component/Slider/SliderConstruction.jsx
@@ -1,61 +1,106 @@
-import React from 'react'
-import { useRef } from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import { useRef } from "react";
+import Slider from "react-slick";
 
 const SliderConstruction = () => {
-    const settings = {
-        dots: true,
-        arrows: false,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        variableWidth: true,
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 580,
-                settings: {
-                    adaptiveHeight: false,
-                    variableWidth: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-        ]
-    };
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 6000,
+    cssEase: "linear",
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          adaptiveHeight: false,
+          variableWidth: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 4500,
+        },
+      },
+    ],
+  };
 
-    const slider1 = useRef(null);
+  const slider1 = useRef(null);
 
-    const previous = () => {
-        slider1.current.slickNext();
-    };
+  const previous = () => {
+    slider1.current.slickNext();
+  };
 
-    const next = () => {
-        slider1.current.slickPrev();
-    };
-    return (
-        <Slider ref={slider1} {...settings} className="wrapper__slider-construction">
-            <div className="items">
-                <img src="./../images/rn (1).png" className='images__construction' alt="" />
-            </div>
-            <div className="items">
-                <img src="./../images/rn (3).png" className='images__construction' alt="" />
-            </div>
-            <div className="items">
-                <img src="./../images/rn (2).png" className='images__construction' alt="" />
-            </div>
-            <div className="items">
-                <img src="./../images/rn (1).png" className='images__construction' alt="" />
-            </div>
-            <div className="items">
-                <img src="./../images/rn (3).png" className='images__construction' alt="" />
-            </div>
-            <div className="items">
-                <img src="./../images/rn (2).png" className='images__construction' alt="" />
-            </div>
-        </Slider>
-    )
-}
+  const next = () => {
+    slider1.current.slickPrev();
+  };
+  return (
+    <div className="wrapper__slider-construction-marquee position-relative">
+      <div className="wrapper__slider-construction-fade wrapper__slider-construction-fade--left" />
+      <div className="wrapper__slider-construction-fade wrapper__slider-construction-fade--right" />
+      <Slider
+        ref={slider1}
+        {...settings}
+        className="wrapper__slider-construction"
+      >
+        <div className="items">
+          <img
+            src="./../images/rn (1).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+        <div className="items">
+          <img
+            src="./../images/rn (3).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+        <div className="items">
+          <img
+            src="./../images/rn (2).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+        <div className="items">
+          <img
+            src="./../images/rn (1).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+        <div className="items">
+          <img
+            src="./../images/rn (3).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+        <div className="items">
+          <img
+            src="./../images/rn (2).png"
+            className="images__construction"
+            alt=""
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
-export default SliderConstruction
+export default SliderConstruction;
